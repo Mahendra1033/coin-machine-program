1b23d69f16ff4c792024035cdbaf3c532554c072
Mahendra1033 <mahendramahiee1033@gmail.com>
2022-05-07 23:56:37 +0530

    Coin Machine Project has been added

---
+++ src/main/java/com/coinmachine/example/CoinMachineController.java
@@ -0,0 +1,13 @@
+package com.coinmachine.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class CoinMachineController
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/coinmachine/example/CoinMachineControllerTest.java
@@ -0,0 +1,14 @@
+package com.coinmachine.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class CoinMachineControllerTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
